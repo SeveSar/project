a4c67cd3ba3d0287e564fd764e4a378dd2b83ad0
Averin Artyom <karlin.koleman@bk.ru>
2020-10-02 18:28:12 +0500

    commit_8

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-const form = document.querySelector("#form-action");
-const dropDownMenu = document.querySelector('#jsDropdown-menu');
-const burger = document.querySelector('#jsBurger');
+// const form = document.querySelector("#form-action");
+// const dropDownMenu = document.querySelector('#jsDropdown-menu');
+// const burger = document.querySelector('#jsBurger');
 
-console.log(burger.className);
-form.addEventListener("submit",function(event){
-    event.preventDefault();
-})
+// console.log(burger.className);
+// form.addEventListener("submit",function(event){
+//     event.preventDefault();
+// })
 
-window.addEventListener('click',function (event){
+// window.addEventListener('click',function (event){
      
-    if (event.target.getAttribute('data-action') == 'jsBurger'){
-        dropDownMenu.style.display = 'block';
-    } else if(event.target != dropDownMenu.get{
-        dropDownMenu.style.display = 'none';
-    }
+//     if (event.target.getAttribute('data-action') == 'jsBurger'){
+//         dropDownMenu.style.display = 'block';
+//     } else if(event.target != dropDownMenu.get{
+//         dropDownMenu.style.display = 'none';
+//     }
 
    
-});
+// });
 
