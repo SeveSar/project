1564d0ea03d46cb9ff67c2791e08da98ab4e0e93
Averin Artyom <karlin.koleman@bk.ru>
2020-10-11 16:17:28 +0500

    commit_11_10_20

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-// const form = document.querySelector("#form-action");
-// const dropDownMenu = document.querySelector('#jsDropdown-menu');
-// const burger = document.querySelector('#jsBurger');
 
-// console.log(burger.className);
+
+
+
 // form.addEventListener("submit",function(event){
+
 //     event.preventDefault();
 // })
 
@@ -18,4 +18,65 @@
 
    
 // });
+
+const form = document.querySelector("#form-action");
+const btnForm = document.querySelector('#jsBtn-Form');
+const input = document.querySelectorAll('[data-action="jsInput"]');
+const dropDownMenu = document.querySelector('#jsDropdown-menu');
+const burger = document.querySelector('[data-action="jsBurger"]');
+const navbarActions = document.querySelector('#jsNavbar__actions');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    for (let i of input) {
+        i.value= '';
+    };
+    alert('Your data has been sent to the server');
+
+});
+
+window.addEventListener('click',function(event){
+
+    if (event.target.classList.contains('burger__line') || event.target.classList.contains('burger')){
+        
+        dropDownMenu.classList.toggle('dropDown-active');
+        
+    } else if (!event.target.classList.contains('dropdown-menu') && !event.target.classList.contains('menu-item')){
+        dropDownMenu.classList.remove('dropDown-active');
+    };
+    
+
+});
+
+
+$(function(){
+
+    let intro = $('#jsIntro');
+    // innerHeight с учётом паддинга
+    let introH = intro.innerHeight();
+    let header = $('#header');
+
+    let scrollPos = $(window).scrollTop();
+    
+    $(window).on('scroll load', function(){
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if (scrollPos >= introH) {
+            header.addClass('fixed');
+        } else{
+            header.removeClass('fixed');
+        }
+        
+        
+    });
+
+    $(window).on('click',function(event){
+        $(input).fadeToggle(2000);
+    })
+    
+
+});  
+
+
 
---
+++ primer.js
@@ -0,0 +1,8 @@
+const menuWrapper = document.querySelector('#menuWrapper');
+
+menuWrapper.onclick = function(){
+
+    this.style.backgroundColor = 'red';
+    
+};
+
