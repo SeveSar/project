addc6106257a01bd2cb0a53ce759d4339fc2ae54
Caesar <caesar@mail.ru>
2020-10-22 15:03:45 +0500

    commit_22_10_20_1

--- main.js
+++ main.js
@@ -3,11 +3,6 @@
 
 
 
-form.addEventListener("submit",function(event){
-
-    event.preventDefault();
-    
-});
 
 
 const form = document.querySelector("#form-action");
@@ -18,7 +13,9 @@ const burger = document.querySelector('[data-action="jsBurger"]');
 const navbarActions = document.querySelector('#jsNavbar__actions');
 
 form.addEventListener('submit',function(event){
+
     event.preventDefault();
+
     for (let i of input) {
         i.value= '';
     };
@@ -43,25 +40,75 @@ window.addEventListener('click',function(event){
 $(function(){
 
     let intro = $('#jsIntro');
+
     // innerHeight с учётом паддинга
     let introH = intro.innerHeight();
+    let featuresH = $("#features").innerHeight();
+    let contentH = $("#content").innerHeight();
+    let workH = $("#work").innerHeight();
+    let keyFutureH = $("#key-feature").innerHeight();
+    let teamH = $("#team").innerHeight();
+    let quickActionH = $("#quick-action").innerHeight();
+    let formActionH = $('#form-action').innerHeight();
+    
     let header = $('#header');
-
     let scrollPos = $(window).scrollTop();
     
-    $(window).on('scroll load', function(){
+    checkScroll (scrollPos, introH);
+    checkTurbo(scrollPos,introH,featuresH,contentH,workH,keyFutureH,quickActionH,teamH);
+    $(window).on('scroll resize', function(){
+      
         introH = intro.innerHeight();
+        featuresH = $("#features").innerHeight();
+        contentH = $("#content").innerHeight();
+        workH = $("#work").innerHeight();
+        keyFutureH = $("#key-feature").innerHeight();
         scrollPos = $(this).scrollTop();
-
-        if (scrollPos >= introH) {
-            header.addClass('fixed');
-        } else{
-            header.removeClass('fixed');
-        }
+        checkScroll (scrollPos, introH);
+        checkTurbo (scrollPos,introH,featuresH,contentH,workH,keyFutureH,quickActionH,teamH);
+        
         
     });
 
-   
+    function checkScroll (scrollPos, introH) {
+        if (scrollPos >= introH) {
+            header.addClass('fixed');
+           
+            
+        } else{
+            header.removeClass('fixed');
+            
+        }
+    }
+
+    function checkTurbo (scrollPos,introH,featuresH,contentH,workH,keyFutureH){
+
+        if (scrollPos>=introH){
+            $("ul .menu-item").children().removeClass('menu-item-link-active').eq(1).addClass('menu-item-link-active');
+            if (scrollPos >= (introH + featuresH)){
+                $("ul .menu-item").children().removeClass('menu-item-link-active').eq(2).addClass('menu-item-link-active');
+                if (scrollPos >= (introH + featuresH + contentH)){
+                    $("ul .menu-item").children().removeClass('menu-item-link-active').eq(3).addClass('menu-item-link-active');
+                    if (scrollPos >= (introH + featuresH + contentH+workH +quickActionH)){
+                        $("ul .menu-item").children().removeClass('menu-item-link-active').eq(4).addClass('menu-item-link-active');
+                        if (scrollPos >= (introH + featuresH + contentH+workH +quickActionH + keyFutureH + teamH)){
+                            $("ul .menu-item").children().removeClass('menu-item-link-active').eq(5).addClass('menu-item-link-active');
+                        }
+                    }
+                }
+            }
+        } 
+        else {
+            $("ul.header-menu .menu-item").children().removeClass('menu-item-link-active').eq(0).addClass('menu-item-link-active');
+        }
+       
+    }
+
+    
+    
+
+
+
 
 });  
 
